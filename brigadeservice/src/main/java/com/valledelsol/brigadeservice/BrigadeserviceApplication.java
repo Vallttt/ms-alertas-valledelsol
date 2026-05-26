8e96b17654553226fcd2063a5f82915257b3f045
WVinet <w.vinet.h@gmail.com>
2026-05-26 11:01:57 -0400

    Preparación de microservicios zone-service y brigade-service

---
+++ brigadeservice/src/main/java/com/valledelsol/brigadeservice/BrigadeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.valledelsol.brigadeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrigadeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrigadeserviceApplication.class, args);
+	}
+
+}
---
+++ brigadeservice/src/test/java/com/valledelsol/brigadeservice/BrigadeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valledelsol.brigadeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrigadeserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
