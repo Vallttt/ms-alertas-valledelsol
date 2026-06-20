435c4ac7699b80b98cd80206f8e527eb107d22e6
Thoorcito <felipe.emanuelb@gmail.com>
2026-06-20 11:24:09 -0400

    feat(incident): registrar incident-service en Eureka

--- incident-service/src/main/java/com/example/incidentes/IncidentesApplication.java
+++ incident-service/src/main/java/com/example/incidentes/IncidentesApplication.java
@@ -2,12 +2,14 @@ package com.example.incidentes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class IncidentesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IncidentesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IncidentesApplication.class, args);
+    }
 
 }
